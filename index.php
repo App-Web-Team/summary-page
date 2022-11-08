10bc04ee4bac59bd732821e62d848935bcf24c6c
md-ameen <mdameenbf@gmail.com>
2022-11-08 15:18:16 +0530

    summary page added

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap 5 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
+    <!-- font awesome -->
+    <script src="https://kit.fontawesome.com/0345135d49.js" crossorigin="anonymous"></script>
+    <!-- google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Noto Sans' rel='stylesheet'>
+    <!-- external css -->
+    <link rel="stylesheet" href="style.css">
+    <title>Webinar Registration</title>
+</head>
+
+<body>
+ 
+<div class="summary-container container py-5">
+    <div class="row justify-content-center">
+        <div class="col-lg-6">
+            <p class="heading text-capitalize">summary</p>
+            <div class="details mt-3">
+                <li>Name - Vanam</li>
+                <li>Email Id - mustaq@gmail.com</li>
+                <li>Occupation - Student</li>
+                <li>Phone - +91 875678765467</li>
+                <li>City - Pondy</li>
+            </div>
+        </div>
+        <div class="col-lg-6 d-flex align-items-end mt-4 mt-lg-0">
+            <button class="btn">Pay Now</button>
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>
